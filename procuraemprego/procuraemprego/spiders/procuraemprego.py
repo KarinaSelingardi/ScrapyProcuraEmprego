6b76490b120cdb9badf0ee5fbb6287258ec76a26
Karina <ka_selingardi@hotmail.com>
2024-04-04 11:15:23 -0300

    new commit

--- procuraemprego/procuraemprego/spiders/procuraemprego.py
+++ procuraemprego/procuraemprego/spiders/procuraemprego.py
@@ -1,15 +1,19 @@
-
 from pathlib import Path
 import scrapy
+import csv
 
 
 class ProcuraempregoSpider(scrapy.Spider):
     name = "procuraemprego"
 
-    start_urls = ["https://empregacampinas.com.br/"]
+    start_urls = ["https://empregacampinas.com.br"]
 
     def parse(self, response):
+        # Seleciona os textos dos links
+        texts = response.css('a , .fysyCG ::text').getall()
 
-        Path ("files/procuraemprego.csv").write_bytes(response.body)
-
-        pass
+        # Escreve os textos em um arquivo CSV
+        with open("files/empregacampinas03.csv", "w", newline="", encoding="utf-8") as csvfile:
+            writer = csv.writer(csvfile)
+            for text in texts:
+                writer.writerow([text])
